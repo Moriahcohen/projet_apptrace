88a7412e62ce4f44ae43efd1633a5981248b6f5b
moriah <moriahzur1@gmail.com>
2020-03-11 11:26:01 +0200

    created the scrapping of the app-database + app_details: paid or free in every country,i n every category.

---
+++ scrapper.py
@@ -0,0 +1,120 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+def get_date_apptrace():
+    """
+    :return: the date of today for our url
+    """
+    date = str(datetime.datetime.today()).split()[0]
+    list_date = date.split('-')
+    list_url = []
+    for i in list_date:
+        list_url.append(i.lstrip('0'))
+    date_url = '-'.join(list_url)
+    return date_url
+
+
+def get_data_by_id(id):
+    """
+    :param id_set:
+    :return: the data of every application according to the id
+    """
+    try:
+        page = requests.get('https://www.apptrace.com/app/' + str(id)).text
+        soup = BeautifulSoup(page, 'lxml')
+        app_detail = soup.find('div', class_='app_details')
+        name = soup.find('li', class_='apptitle').h1.text
+        developer = soup.find('li', class_='apptitle').h2.a.span.text
+        #create new dictinnary with all the data of the applciation
+        dic = {}
+        dic['Name app'] = name
+        dic['Developer'] = developer
+        dic['Price'] = soup.find('li', class_='apptype appstore').text
+        for detail in app_detail.find_all('div', class_='infobox'):
+            dic[(detail.find('p', class_='title').text)] = (detail.find('p', class_='data').text)
+        row_rating = app_detail.find('div', class_='row rating')
+        dic['Rating'] = row_rating.find('strong', itemprop='ratingValue').text
+        dic['Ratings Count'] = row_rating.find('span', itemprop='ratingCount').text
+        dic['Description'] = app_detail.find('div', class_='t1c active_language').text
+        count = 0
+        for detail in soup.find('div', class_='table versions opened').find_all('div', class_="cell"):
+            #print(detail.text)
+            count += 1
+        dic['Number of versions'] = int(count / 2)
+
+        #print the dictionnary
+        for k, v in dic.items():
+            print(k, ":", v)
+        print()
+    except Exception as e:
+        print(e)
+
+#create a new set of id of application so we won't get any deplicates
+id_app_set = {1031002863}
+
+def get_app_id_by_category_by_country(dictionary_countries,dictionary_categories):
+    """
+    :param dictionary_countries: the dictionarry of key = country and value = code internet - data-link
+    :param dictionary_categories: the dictionary of keys = data-link of every category ,  the values = categories name
+    :return:
+    """
+    paid_or_free = ['free', 'paid']
+    for cost in paid_or_free:
+        for key in dictionary_categories.keys():
+            for country in dictionary_countries.keys():
+                try:
+                    page = requests.get('https://www.apptrace.com/Itunes/charts/' + dictionary_countries[country] + '/top' + cost + 'applications/' + str(key) + '/2020-3-9').text
+                    soup = BeautifulSoup(page, 'lxml')
+                    for tag in soup.find_all('div', class_ ='cell linked app_cell'):
+                        id_tag = int(tag.find('div', class_='id').get('id'))
+                        if id_tag not in id_app_set:
+                            id_app_set.add(id_tag)
+                            print(country, dictionary_categories[key], cost)
+                            get_data_by_id(id_tag)
+                            print(len(id_app_set))
+                except Exception as e:
+                       print(e)
+
+def get_country_dic():
+    """
+    :return: a dictionary of all the countries we want to scrap application data.
+    keys are countries and values are the data link : code internet of the country (ex: il)
+    """
+    url = requests.get('https://www.apptrace.com/charts')
+    country_chart_page = BeautifulSoup(url.content, 'lxml').find('ul', class_='countryselect')
+    dict_country = {}
+    for country in country_chart_page.find_all('li', class_='visible'):
+        dict_country[country.text] = country.a.get('data-link')
+    for country in country_chart_page.find_all('li', class_='invisible'):
+        dict_country[country.text] = country.a.get('data-link')
+    return dict_country
+
+
+def get_category_dic():
+    """
+    :return: a dictionary : keys are the data-link of every category and the values are the categories name
+    """
+    url = requests.get('https://www.apptrace.com/charts')
+    country_chart_page = BeautifulSoup(url.content, 'lxml')
+    dic_category = {}
+    for selector in country_chart_page.find_all('a', class_='genre_selector'):
+        dic_category[selector.get('data-link')] = selector.text
+        if selector.text == 'Weather':
+            break
+    # delete all the sub-categories of magazines and newspapers because there is not data there
+    list_to_delete = list(range(31, 49)) + list(range(11, 20)) + [54]
+    for num in list_to_delete:
+        del dic_category[str(num)]
+    return dic_category
+
+
+def main():
+    dictionary_countries = get_country_dic()
+    dictionary_categories = get_category_dic()
+    get_app_id_by_category_by_country(dictionary_countries,dictionary_categories)
+
+
+if __name__ == "__main__":
+    main()
+
